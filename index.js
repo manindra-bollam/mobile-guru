f1b6432642f886ed8c6a84d2b04761087c1b0a2e
Manindra <manindra8@gmail.com>
2025-11-25 10:05:40 +0530

    fix: send file issue

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.send("index.html");
 });
 
 app.post("/chat", async (req, res) => {
